6f68329ce60770d8ac988dad8665ce340d570f8f
saksham <sakshamagrawal18@gmail.com>
2023-08-15 23:52:32 +0530

    final commit

--- src/component/NewsLetter.jsx
+++ src/component/NewsLetter.jsx
@@ -5,12 +5,12 @@ const NewsLetter = () => {
     <div className="w-full py-16 text-white px-4">
       <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
         <div className="lg:col-span-2">
-          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-3">
             Want tips & tricks to optimize your flow ?
           </h1>
           <p>SignUp to our newsletter and stay up to date.</p>
         </div>
-        <div className="my-4">
+        <div className="my-2">
           <div className="flex flex-col sm:flex-row items-center justify-between w-full">
             <input
               className="p-3 flex w-full rounded-md text-black"
